459855da1642fdabd08bce7ac45c6b43608b024e
hamed <hamed@layer6.ai>
2018-10-09 11:07:51 -0400

    Third commit

---
+++ kk_mimic_dataset.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 10:40:35 2018
+
+@author: hamed
+"""
+
+import torch
+from torch.utils import data
+#import numpy as np
+import sklearn.datasets as datasets
+#import matplotlib.pyplot as plt
+
+
+class kk_mimic_dataset(data.Dataset):
+    
+    def __init__(self, training=True):
+        if training: 
+            data_path = "../mimic-libsvm/" + "PATIENTS_SPLIT_XGB_TRAIN"
+        else:
+            data_path = "../mimic-libsvm/" + "PATIENTS_SPLIT_XGB_VALID"
+            
+        data = datasets.load_svmlight_file(data_path)
+        self.features = data[0].todense()
+        self.labels = data[1]
+        
+        # Removing last irrelevant features
+        self.temporal_features = self.features[:,:14400]
+        self.fixed_features = self.features[:,14400:]                
+        
+        print(self.features.shape)
+        
+    def __getitem__(self, index):
+        return self.temporal_features[index], self.labels[index], self.fixed_features[index]
+    
+    def __len__(self):
+        return self.labels.shape[0]
+    
+#%% Data loader
+        
+def loader(dataset, training=True, batch_size=64, shuffle=True, num_workers=1):
+    params = {'batch_size': batch_size, 'shuffle': shuffle, 'num_workers':num_workers}
+    return data.DataLoader(dataset, **params)
+                
+
+#%% Test dataloader
+
+#training_set = kk_mimic_dataset()
+#data_loader = loader(training_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ transformer/Layers.py
@@ -0,0 +1,49 @@
+''' Define the Layers '''
+import torch.nn as nn
+from transformer.SubLayers import MultiHeadAttention, PositionwiseFeedForward
+
+__author__ = "Yu-Hsiang Huang"
+
+
+class EncoderLayer(nn.Module):
+    ''' Compose of two layers '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        self.slf_attn = MultiHeadAttention(
+            n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
+        enc_output, enc_slf_attn = self.slf_attn(
+            enc_input, enc_input, enc_input, mask=slf_attn_mask) # for encoder, Q, V and K are all inputs!
+        enc_output *= non_pad_mask  #?? Multplication by None give error, Neh?
+
+        enc_output = self.pos_ffn(enc_output)
+        enc_output *= non_pad_mask  #??
+
+        return enc_output, enc_slf_attn
+
+
+class DecoderLayer(nn.Module):
+    ''' Compose of three layers '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.enc_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    def forward(self, dec_input, enc_output, non_pad_mask=None, slf_attn_mask=None, dec_enc_attn_mask=None):
+        dec_output, dec_slf_attn = self.slf_attn(
+            dec_input, dec_input, dec_input, mask=slf_attn_mask)
+        dec_output *= non_pad_mask #mask 1
+
+        dec_output, dec_enc_attn = self.enc_attn(
+            dec_output, enc_output, enc_output, mask=dec_enc_attn_mask) # Q: dec_out K,V : enc_out 
+        dec_output *= non_pad_mask #mask 2
+
+        dec_output = self.pos_ffn(dec_output)
+        dec_output *= non_pad_mask #mask 3
+
+        return dec_output, dec_slf_attn, dec_enc_attn
